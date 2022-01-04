1d1722142602043e158acc3e319415a0a5266a26
unknown <al221910878@gmail.com>
2022-01-04 10:55:51 -0600

    Imagenes, Plantilla de Diseño e Iconos

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.app', ['activePage' => 'register', 'title' => 'Registro | Mausoleo Santa Clara'])
+
+@section('content')
+    <div class="full-page register-page section-image" data-color="orange" data-image="{{ asset('img/bg5.jpg') }}">
+        <div class="content">
+            <div class="container">
+                <div class="card card-register card-plain text-center">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-md-5 ml-auto">
+                                <div class="media">
+                                    <div class="media-left">
+                                        <div class="icon">
+                                            <i class="nc-icon nc-circle-09"></i>
+                                        </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4>{{ __('Solo Personal Autorizado') }}</h4>
+                                        <p>{{ __('Este apartado es unico del Staff de Mausoleo Santa Clara, si deseas ingresar al portal de clientes, da click aqui para acceder.') }}</p>
+                                    </div>
+                                </div>
+                                <div class="media">
+                                    <div class="media-left">
+                                        <div class="icon">
+                                            <i class="nc-icon nc-send"></i>
+                                        </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4>{{ __('Regístrate') }}</h4>
+                                        <p>{{ __('Ingresa tus datos, para continuar con el registro, verifica que los datos sean correctos para continuar con el registro.') }}</p>
+                                    </div>
+                                </div>
+                                <div class="media">
+                                    <div class="media-left">
+                                        <div class="icon">
+                                            <i class="nc-icon nc-lock-circle-open"></i>
+                                        </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4>{{ __('Al terminar...') }}</h4>
+                                        <p>{{ __('Solicita el acceso a la autoridad correspondiente y en unos momentos podras iniciar.') }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 mr-auto">
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
+                                    <div class="card card-plain">
+                                        <div class="content">
+                                            <div class="form-group">
+                                                <input type="text" name="name" id="name" class="form-control" placeholder="{{ __('Nombre') }}" value="{{ old('name') }}" required autofocus>
+                                            </div>
+
+                                            <div class="form-group">   {{-- is-invalid make border red --}}
+                                                <input type="email" name="email" value="{{ old('email') }}" placeholder="Correo electrónico" class="form-control" required>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <input type="password" name="password" class="form-control" required >
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" name="password_confirmation" placeholder="Valdar contraseña" class="form-control" required autofocus>
+                                            </div>
+                                            <div class="form-group d-flex justify-content-center">
+                                                <div class="form-check rounded col-md-10 text-left">
+                                                    <label class="form-check-label text-white d-flex align-items-center">
+                                                        <input class="form-check-input" name="agree" type="checkbox" required >
+                                                        <span class="form-check-sign"></span>
+                                                        <b>{{ __('Acepto terminos y condiciones') }}</b>
+                                                    </label>
+                                                </div>
+                                            </div>
+
+                                            <div class="footer text-center">
+                                                <button type="submit" class="btn btn-fill btn-neutral btn-wd">{{ __('Solicitar Acceso') }}</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col">
+                                @foreach ($errors->all() as $error)
+                                    <div class="alert alert-warning alert-dismissible fade show" >
+                                        <a href="#" class="close" data-dismiss="alert" aria-label="close"> &times;</a>
+                                        {{ $error }}
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('js')
+    <script>
+        $(document).ready(function() {
+            demo.checkFullPageBackgroundImage();
+
+            setTimeout(function() {
+                // after 1000 ms we add the class animated to the login/register card
+                $('.card').removeClass('card-hidden');
+            }, 700)
+        });
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+
+
+// Rutas con controlador (Aun no formadas)
+Route::get('/register', function () {return view('auth.register');})->name('register');
+Route::get('/login', function () {return view('auth.login');})->name('loginuser');
+
+//Envio de verificación
+Route::get('/verify', function () {return view('auth.verify');})->name('verify');
+
+//Reenvio de confirmación (sin función)
+Route::get('/verify', function () {return view('auth.verify');})->name('verification.resend');
+
+//Administrativos CRUD
+Route::get('/user', function () {return view('users.index');})->name('user.index');
+Route::get('/profile.edit', function () {return view('profile.edit');})->name('profile.edit');
+Route::get('/page.index', function () {return view('page.index');})->name('page.index');
+Route::get('/profile.update', function () {return view('profile.update');})->name('profile.update');
