020cf29e3649dff355fa7c1f1fcb21a7342232c2
Eber Palma <al221911577@gmail.com>
2022-01-05 21:52:00 -0600

    Cambios en BD con el login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,67 +1,68 @@
-@extends('layouts.app')
+@extends('layouts/app', ['activePage' => 'login', 'title' => 'Login | Mausoleo Santa Clara'])
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+    <div class="full-page section-image" data-color="black" data-image="{{ asset('img/full-screen-image-2.jpg') }}">
+        <div class="content pt-5">
+            <div class="container mt-5">    
+                <div class="col-md-4 col-sm-6 ml-auto mr-auto">
+                    <form class="form" method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="card card-login card-hidden">
+                            <div class="card-header ">
+                                <h3 class="header text-center">{{ __('Inicio de Sesión') }}</h3>
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                            <div class="card-body ">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="email" class="col-md-6 col-form-label">{{ __('Correo electrónico') }}</label>
+            
+                                        <div class="col-md-14">
+                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', 'admin@lightbp.com') }}" required autocomplete="email" autofocus>
+            
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="password" class="col-md-6 col-form-label">{{ __('Contraseña') }}</label>
+                
+                                            <div class="col-md-14">
+                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password', 'secret') }}" required autocomplete="current-password">
+                
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="form-check">
+                                                <label class="form-check-label d-flex align-items-center">
+                                                    <input class="form-check-input" type="checkbox" name="remember"  id="remember">
+                                                    <span class="form-check-sign"></span>
+                                                    {{ __('Recuerdame') }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                <div class="card-footer ml-auto mr-auto">
+                                    <div class="container text-center" >
+                                        <button type="submit" class="btn btn-warning btn-wd">{{ __('Iniciar Sesión') }}</button>
+                                    </div>
+                                    <div>
+                                        
+                                        <a class="btn btn-link"  style="color:#23CCEF; margin-left:25%" href="{{ route('login') }}">
+                                        {{ __('Olvidaste tu contraseña?') }}
+                                        </a>
+                                    
+                                       
+                                    </div>
+                                    
+                                </div>
                             </div>
                         </div>
                     </form>
@@ -69,5 +70,17 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
+
+@push('js')
+    <script>
+        $(document).ready(function() {
+            demo.checkFullPageBackgroundImage();
+
+            setTimeout(function() {
+                // after 1000 ms we add the class animated to the login/register card
+                $('.card').removeClass('card-hidden');
+            }, 700)
+        });
+    </script>
+@endpush
--- resources/views/users/listausuarios.blade.php
+++ resources/views/users/listausuarios.blade.php
@@ -44,16 +44,18 @@
                                     <th>Actions</th>
                                 </tr>
                             </tfoot>
-                            <tbody id="tableBody">
-                                <tr>
-                                    <td>Admin Admin</td>
-                                    <td>admin@lightbp.com</td>
-                                    <td>2020-02-25 12:37:04</td>
-                                    <td class="d-flex justify-content-end">            
-                                        <a href="#"><i class="fa fa-edit"></i></a>
-                                    </td>
-                                </tr>
-                            </tbody>
+                            <tbody>
+                            
+                                                                        <tr>
+                                        <td>Admin Admin</td>
+                                        <td>admin@lightbp.com</td>
+                                        <td>2020-02-25 12:37:04</td>
+                                        <td class="d-flex justify-content-end">
+                                                
+                                                <a href="#"><i class="fa fa-edit"></i></a>
+                                                                                        </td>
+                                    </tr>
+                                                                </tbody>
                         </table>
                     </div>
                 </div>
@@ -69,19 +71,3 @@
     </div>
 </div>
 @endsection
-
-@push('js')
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', ()=>{
-            let idSession = localStorage.mausoleoSessionId_session;
-            axios.post('/api/sessionvalidator'+idSession
-                ).then((response)=>{
-                    console.log(response);
-                }).catch(()=>{
-                    location.replace('/login');
-                });
-        });
-    </script>
-    <script src="{{asset('js/userIndexTable.js')}}"></script>
-@endpush
